2bf70c2018fa6a706999f9109baf982de5a2a059
D4niel <danigaara.ds@gmail.coml>
2022-11-05 15:48:06 -0300

    connecting to DB, Atualizacao sobre nos, tela home e tela entrar

--- projeto-rfid/projeto-rfid/Controllers/HomeController.cs
+++ projeto-rfid/projeto-rfid/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace projeto_rfid.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Sobre()
         {
             return View();
         }
