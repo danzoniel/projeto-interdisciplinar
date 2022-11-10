04db0dd3dc8e359e569049a9ec6d7f542b6dad8b
D4niel <danigaara.ds@gmail.coml>
2022-11-10 13:29:03 -0300

    Adicionado ProfessorDAO.cs, ProfessorViewModel.cs, View>Professor, Listagem de professor e entidade para professor no BD

---
+++ projeto-rfid/projeto-rfid/Models/ProfessorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace projeto_rfid.Models
+{
+    public class ProfessorViewModel
+    {
+        public int IdProfessor { get; set; }
+        public string NomeProfessor { get; set; }
+
+    }
+}
